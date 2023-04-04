949ef84fcb98be45735c4782f45adb89ba0fd274
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-04 10:55:54 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.46 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,13 @@
+def solution(A,B):
+    answer = 0
+    number_dictionary = {}
+    # 정렬, 최솟값 구할 준비 
+    A.sort()
+    B.sort(reverse=True)
+    
+    for i in range(1,(len(A)+1)):
+        A_num = A.pop()
+        B_num = B.pop()
+        # A와B 에서 각각 하나씩 뽑아서 곱하는걸 누적
+        answer += A_num * B_num
+    return answer
