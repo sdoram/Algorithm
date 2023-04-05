715a19fbb8c312747962d11e4e8f6fff862c4f9b
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-05 11:17:33 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,8 @@
+a_num = int(input())
+b_num = int(input())
+str_b_num = str(b_num)
+
+print(a_num * int(str_b_num[2]))
+print(a_num * int(str_b_num[1]))
+print(a_num * int(str_b_num[0]))
+print(a_num * b_num)
