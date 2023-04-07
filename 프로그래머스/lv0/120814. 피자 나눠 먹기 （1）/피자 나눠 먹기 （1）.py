2d4f96f71224bc212125da3f0111e5308904f96e
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-07 10:21:41 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,5 +1,11 @@
 def solution(n):
-    answer = 0
-    while n / 7 > answer:
-        answer += 1
-    return answer
+    # while문의 조건식 not*7 pizza //n은 
+    # 7*pizza //n이 0이 아닐때까지 반복한다. 
+    # 이때 7*pizza //n은 pizza 판의 피자를 
+    # n명이 나누어 먹을때 각 사람당 받게 되는 조각 수 
+    # 따라서 7*pizza // n 이 0이 아닌 경우에 모든 사람이 
+    # 최소한 한 조각 이상 먹을 수 있는 피자를 구할수 있다. 
+    pizza = 1
+    while 7 * pizza // n == 0:
+        pizza += 1 
+    return pizza
