11c175f884a5535be9215026e9d4d48af87b346e
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-07 09:14:13 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,15 +1,5 @@
 def solution(n):
-    # n이 사람
-    # 조건문으로 사람 수와 7조각을 비교
-    # answer = 1 시작
-    # 사람이 조각보다 많거나 같으면 answer += 1
-    # 조각이 사람보다 많으면 answer 출력
-    # 반복문 
-    # anwer에 * 7조각수 / n >= 1:
-    answer = 1
-    
-    while True:
-        if 7 * answer / n >= 1:
-            return answer
-        else:
-            answer += 1
+    answer = 0
+    while n / 7 > answer:
+        answer += 1
+    return answer
