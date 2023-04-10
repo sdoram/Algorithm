9fbcea3b248c3a57e9435eec92c1ac6639bc9c5d
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-10 10:05:14 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,11 +1,8 @@
-count = 0
 max_num = 0
-while count < 9:
-    count += 1
+for index in range(1, 10):
     a = int(input())
     if max_num < a:
         max_num = a
-        num_count = count
-
+        num_count = index
 print(max_num)
 print(num_count)
