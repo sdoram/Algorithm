83bf70a61f469ae4f629ec6e27518ffdd38f23cc
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-19 19:08:11 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,13 +1,11 @@
+# 혼자 풀었을 때 코드 리팩토링
 def solution(num, k):
-    # num , k는 int 
-    # return은 num에 k가 있으면 자릿수 return,
-    # 없으면 -1 return 
-    # num 과 k 를 str 변환하고
-    # index
-    str_num = str(num)
-    str_k = str(k)
-    try:  
-        answer = str_num.index(str_k)+1
-    except ValueError:
-        answer = -1
+    answer = -1
+    num = str(num)
+    # 반복문으로 n과 k가 일치하는 순간을 찾음
+    for i, n in enumerate(num, 1):
+        if int(n) == k:
+            answer = i
+            break
+
     return answer
