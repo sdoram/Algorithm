87dc11000f7e3404787ab8f02b31b7d4f083e27e
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-20 09:43:22 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,6 +1,8 @@
 def solution(dots):
     # dots = [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
-    x = [dots[x][0] for x in range(len(dots))]
-    y = [dots[y][1] for y in range(len(dots))]
-    answer = (max(x) - min(x)) * (max(y) - min(y))
-    return answer
+    # return 은 직사각형의 넓이
+    # 1,1 -1,-1 == 1 -1 차이만 구하면 됩니다.
+    # 가장 큰 값 - 가장 작은 값 == 길이 1 - -1 == 2
+    # for , sort, 
+    # max, min 
+    return (max(dots)[0] -min(dots)[0]) * (max(dots)[1] - min(dots)[1])
