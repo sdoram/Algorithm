9de040e0ce2bf4fe4ae5b0fc3057d96dc70b2ddb
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-21 12:31:32 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,14 +1,6 @@
 def solution(num, total):
-    # while문으로 +1씩 계속 증가
-    # for문으로 num만큼 반복
-    # count의 정확한 초기값을 어떻게 주지?
-    count =  -1000
-    while True:
-        answer = []
-        sum_num = 0
-        for i in range(count, count+num):
-            sum_num += i
-            answer.append(i)
-        if total == sum_num:
-            return answer
-        count += 1
+    answer = []
+    start = total//num -(num-1)//2
+    for i in range(start, start+num):
+        answer.append(i)
+    return answer
