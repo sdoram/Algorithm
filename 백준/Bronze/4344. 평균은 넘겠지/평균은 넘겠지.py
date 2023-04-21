ca48cc9ac719a3fe65eb6989866df77da78524ec
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-21 12:47:36 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,15 +1,8 @@
-def add(*args):
-    total_score = 0
-    for i in args:
-        total_score += i
-    return total_score
-
-
 a = int(input())
 for _ in range(a):
     scores = list(map(int, (input().split())))
     students = scores.pop(0)
-    total_score = add(*scores)
+    total_score = sum(scores)
     average_score = total_score // students
     over_score_raito = format(
         len([i for i in scores if i > average_score]) / len(scores) * 100, ".3f")
