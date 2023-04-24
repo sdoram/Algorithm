917340598798a4376044ba8ca02a0964e0b3ab39
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-24 20:03:08 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,6 +1,9 @@
-import math
 def solution(n):
-    # n은 사람 수 
-    # return은 모든 사람이 피자를 한 조각 이상 먹는 피자의 수
-    # n에 따라서 피자 수를 조절해야 함
-    return math.ceil(n/7)
+    answer = 0
+
+    if n % 7 > 0:
+        answer = n // 7 + 1
+    else:
+        answer = n // 7
+        
+    return answer
