7b3866162f48e4a70635af1760d3dae644a89d12
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-25 07:52:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,17 +1,10 @@
-five_num = 0
-a = int(input())
+three = 0
+kg = int(input())
 min_answer = -1
-for _ in range(a):
-    kg = a
-    kg -= 5*five_num
-    if kg < 0:
+while kg >= 0:
+    if kg % 5 == 0:
+        min_answer = kg//5+three
         break
-    three = kg // 3
-    if kg > 2 and (kg % 3 == 0 or kg % 5 == 0):
-        current_answer = five_num+three
-        if min_answer < 0:
-            min_answer = current_answer
-        elif min_answer > 0 and min_answer > current_answer:
-            min_answer = current_answer
-    five_num += 1
+    kg -= 3
+    three += 1
 print(min_answer)
