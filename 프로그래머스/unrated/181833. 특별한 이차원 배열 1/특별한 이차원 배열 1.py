6568b290d8f6849f2d495f3b07cad08aae185c2c
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-27 16:36:40 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -2,7 +2,8 @@ def solution(n):
     # n은 정수
     # return은 n*n의 이차원 배열
     # arr[i][j]의 값이 일치하는 부분의 원소는 1, 아니면 0
-    two_dimensional_array = [[0]*n for i in range(n)]
-    for i, j in enumerate(two_dimensional_array):
-        j[i] = 1
+    # [[0]*n]*n으로 생성시 얕은 복사로 다른 원소 조작시 같은 인덱스의 원소 모두 변경 
+    two_dimensional_array = [[0]*n for _ in range(n)]
+    for j, i in enumerate(two_dimensional_array):
+        i[j] = 1
     return two_dimensional_array
