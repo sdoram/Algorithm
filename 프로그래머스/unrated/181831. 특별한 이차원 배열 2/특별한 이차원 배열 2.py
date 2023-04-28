32628c1e996293399da7bae2d3d91be572ef9b34
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-28 17:27:50 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -1,10 +1,6 @@
 def solution(arr):
-    # n*n크기의 이차원 배열 arr
-    # return은 arr[i][j] = arr[j][i] 이면 1 아니면 0 return
-    # for문, 조건문을 통해서 i와 j 비교
-    # print(len(arr))
     for i in range(len(arr)):
-        for j in range(len(arr)):
+        for j in range(i+1, len(arr)):
             if arr[i][j] != arr[j][i]:
                 return 0
     return 1
