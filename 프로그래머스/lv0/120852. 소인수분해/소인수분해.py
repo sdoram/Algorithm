657127c82606bbe00e6ef72b3885df1c14c7db5e
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-28 11:46:17 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    i = 2
+    while n >= i:
+        if n % i == 0:
+            n = n//i
+            answer.append(i)
+            continue
+        i += 1
+    answer = list(set(answer))
+    answer.sort()
+    return answer
