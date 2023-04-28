c763799f015a5e8c5a22530134c1746f8af4fcce
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-04-28 16:48:56 +0900

    [Silver V] Title: 뒤집기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,19 +1,14 @@
 bin_str = input()
-zero_str_list = []
-one_str_list = []
-num_list = []
-for num in range(len(bin_str)):
-    if num != 0 and bin_str[num-1] != bin_str[num] and bin_str[num-1] == '0':
-        zero_str_list.append(num_list)
-        num_list = []
-    elif num != 0 and bin_str[num-1] != bin_str[num] and bin_str[num-1] == '1':
-        one_str_list.append(num_list)
-        num_list = []
-    if num == len(bin_str)-1 and bin_str[num] == '0':
-        zero_str_list.append(num_list)
-    elif num == len(bin_str)-1 and bin_str[num] == '1':
-        one_str_list.append(num_list)
-    num_list.append(bin_str[num])
+zero_bin_str_list = bin_str.split('0')
+one_bin_str_list1 = bin_str.split('1')
+reverse_one = 0
+reverse_zero = 0
 
-print(len(one_str_list) if len(one_str_list) <
-      len(zero_str_list) else len(zero_str_list))
+for s in zero_bin_str_list:
+    if s != '':
+        reverse_one += 1
+
+for s in one_bin_str_list1:
+    if s != '':
+        reverse_zero += 1
+print(reverse_zero if reverse_zero <= reverse_one else reverse_one)
