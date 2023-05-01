6cce4d0c5f17f0385cd97de8fb9e7fed2ba088cc
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-05-01 09:15:15 +0900

    [Bronze IV] Title: 모음의 개수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -0,0 +1,10 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+while True:
+    count = 0
+    word = input()
+    if word == '#':
+        break
+    for w in word.lower():
+        if w in vowels:
+            count += 1
+    print(count)
