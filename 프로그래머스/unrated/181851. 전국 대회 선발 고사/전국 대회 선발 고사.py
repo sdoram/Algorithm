906db62b93b844d2e8333fe72efa52d1102896b8
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-05-03 17:05:03 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,3 +1,13 @@
 def solution(rank, attendance):
-    a_list = [rank.index(r) for r in sorted(rank) if attendance[rank.index(r)] == True]
+    # rank는 등수, attendance는 참석 여부
+    # 1등부터 차례대로 참석가능한 3명 구하기
+    a_list = []
+    # 오름차순으로 1등부터 확인
+    for r in sorted(rank):
+        print(r, rank.index(r))
+        # attendance의 [r의index와 동일한 index]가 True라면
+        if attendance[rank.index(r)]:
+            # index 번호를 추가 
+            a_list.append(rank.index(r))
+        # True인 index만 추가했기 때문에 0번부터 2번까지 뽑아서 사용
     return a_list[0]*10000+a_list[1]*100+a_list[2]
