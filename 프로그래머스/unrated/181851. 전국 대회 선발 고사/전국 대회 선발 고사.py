eb102ddb8dc2a33e4f1bce89f9ca42a3aac5ed4a
Seman_Kim <108051577+sdoram@users.noreply.github.com>
2023-05-03 10:16:55 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,4 +1,3 @@
 def solution(rank, attendance):
-    a_list = []
-    [a_list.append(rank.index(r)) for r in sorted(rank) if attendance[rank.index(r)] == True]
+    a_list = [rank.index(r) for r in sorted(rank) if attendance[rank.index(r)] == True]
     return a_list[0]*10000+a_list[1]*100+a_list[2]
