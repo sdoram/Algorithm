93bbc2d988783b4d7c6a0e69ccc8605b4bd7fe00
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-06 19:48:55 +0900

    [Bronze I] Title: 세준세비, Time: 328 ms, Memory: 40416 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.py
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -1,21 +1,13 @@
-import sys
 T = int(input())
+
 for _ in range(T):
     input()
     N, M = map(int, input().split())
-    N_power = sorted(
-        list(map(int, sys.stdin.readline().lstrip().rstrip().split())))
-    M_power = sorted(
-        list(map(int, sys.stdin.readline().lstrip().rstrip().split())))
-    while N and M:
-        if N_power[0] == M_power[0]:
-            M_power.pop(0)
-            M -= 1
+    N_power = sorted(list(map(int, input().split())), reverse=True)
+    M_power = sorted(list(map(int, input().split())), reverse=True)
+    while len(N_power) != 0 and len(M_power) != 0:
+        if N_power[-1] == M_power[-1]:
+            M_power.pop()
             continue
-        if N_power[0] < M_power[0]:
-            N_power.pop(0)
-            N -= 1
-        else:
-            M_power.pop(0)
-            M -= 1
-    print('S' if N != 0 else 'B')
+        N_power.pop() if N_power[-1] < M_power[-1] else M_power.pop()
+    print('S' if len(N_power) != 0 else 'B')
