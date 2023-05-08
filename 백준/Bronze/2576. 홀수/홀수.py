1411f686429a0090cd9bd9415638d834c2d4b5e1
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-08 09:36:16 +0900

    [Bronze III] Title: 홀수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -0,0 +1,9 @@
+num_list = []
+for _ in range(7):
+    num_list.append(int(input()))
+num_list = sorted([num for num in num_list if num % 2 != 0])
+if len(num_list) == 0:
+    print(-1)
+else:
+    print(sum(num_list))
+    print(num_list[0])
