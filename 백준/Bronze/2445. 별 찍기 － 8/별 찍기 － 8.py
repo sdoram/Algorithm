0e0530aa3f9654c7cf4f2816641de7caf7fd5882
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-08 13:50:44 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -1,6 +1,8 @@
 N = int(input())
-for n in range(1, N):
-    print(('*'*n)+(' '*(2*(N-n)))+('*'*n))
-print('*'*N*2)
 for n in range(1, N)[::-1]:
-    print(('*'*n)+(' '*(2*(N-n)))+('*'*n))
+    n = ' '*(2*n)
+    print(f"{n:*^{N*2}}")
+print('*'*N*2)
+for n in range(1, N):
+    n = ' '*(2*n)
+    print(f"{n:*^{N*2}}")
