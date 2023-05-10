bc32c3cd1c71de97cba86b0908205625be6e18a3
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-10 13:31:36 +0900

    [Silver V] Title: 점수 계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2822. 점수 계산/점수 계산.py
+++ 백준/Silver/2822. 점수 계산/점수 계산.py
@@ -1,6 +1,4 @@
-score_list = []
-for num in range(1,9):
-    score_list.append([int(input()),num])    
+score_list = [[int(input()),num] for num in range(1,9)]
 five_scores = sorted(score_list, reverse=True)[:5]
 print(sum([score[0] for score in five_scores]))
 print(*sorted([score[1] for score in five_scores]))
