8797efdf3abd228da4d85c5b432314c65d772221
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-12 18:51:00 +0900

    [Bronze III] Title: 배수와 약수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,12 @@
+import sys
+
+while True:
+    num_list = list(map(int, sys.stdin.readline().split()))
+    if sum(num_list) == 0:
+        break
+    if num_list[1] % num_list[0] == 0:
+        print("factor")
+    elif num_list[0] % num_list[1] == 0:
+        print("multiple")
+    else:
+        print("neither")
