442d6f4ea4bdaa02012712aa93043a24ddcebf48
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-13 23:06:24 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,7 @@
+N, K = map(int, input().split())
+num = 0
+coin_list = sorted([int(input()) for _ in range(N)], reverse=True)
+for coin in coin_list:
+    num += K // coin
+    K %= coin
+print(num)
