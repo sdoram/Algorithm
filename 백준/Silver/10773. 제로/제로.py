573e258ef31ff75fcbe6eb451ab829328e324c76
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-13 11:53:44 +0900

    [Silver IV] Title: 제로, Time: 84 ms, Memory: 32036 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,7 @@
+import sys
+
+num_list = []
+for _ in range(int(sys.stdin.readline())):
+    num = int(sys.stdin.readline())
+    num_list.pop() if not num else num_list.append(num)
+print(sum(num_list))
