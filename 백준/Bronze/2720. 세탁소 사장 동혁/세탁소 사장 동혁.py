26f3d152bdef0729b81db03353d579d394ed5371
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-13 23:01:34 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    C = int(input())
+    num_list = []
+    Q, D, N, P = 25, 10, 5, 1
+    num_list.append(C // Q)
+    C = C % Q
+    num_list.append(C // D)
+    C = C % D
+    num_list.append(C // N)
+    C = C % N
+    num_list.append(C // P)
+    C = C % P
+    print(*num_list)
