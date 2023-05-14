583fcf1b436c0dadd79a0ed6694aa255e141e63c
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-14 17:51:17 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 416 ms, Memory: 52812 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,5 +1,7 @@
-N = int(input())
-dot_list = sorted([list(map(int, input().split())) for _ in range(N)])
+import sys
+
+N = int(sys.stdin.readline())
+dot_list = sorted([list(map(int, sys.stdin.readline().split())) for _ in range(N)])
 dot_list = sorted(dot_list, key=lambda x: x[1])
 for dot in dot_list:
     print(*dot)
