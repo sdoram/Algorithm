ee3b97fc2ce9f06cc5574236b23ec523810b90be
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-14 18:06:40 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.py
@@ -0,0 +1,6 @@
+L, P = map(int, input().split())
+person_list = list(map(int, input().split()))
+error_list = []
+for person in person_list:
+    error_list.append(person - (L * P))
+print(*error_list)
