0017aeb328ac85e3ae709a73a7ba9ad5e9d5abbf
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-15 16:28:18 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -0,0 +1,6 @@
+word = input()
+croatia_word_list = ["dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z="]
+for croatia in croatia_word_list:
+    if croatia in word:
+        word = word.replace(croatia, "1")
+print(len(word))
