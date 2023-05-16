20dea77ed0d6a38324192723a72f7040ca9348b7
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-16 16:01:28 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,3 +1,3 @@
 def solution(s):
-    median = (len(s)-1) // 2+1
-    return s[median-1: median+1] if len(s) % 2 == 0 else s[median-1] 
+    median = (len(s)-1) // 2
+    return s[median:median+2] if len(s) % 2 == 0 else s[median] 
