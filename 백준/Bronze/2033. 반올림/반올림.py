3878096cc96cdd413369b475b8869e2d62e2a907
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-17 17:23:28 +0900

    [Bronze I] Title: 반올림, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2033. 반올림/반올림.py
+++ 백준/Bronze/2033. 반올림/반올림.py
@@ -1,7 +1,12 @@
 N = int(input())
-for i, n in enumerate(range(1, len(str(N))), 1):
+# range(1,로 가장 앞자리는 제외하기)
+for i in range(1, len(str(N))):
+    # N % 10**i 해당하는 자리 수 뽑아오기
+    # 10 ** (i - 1)로 일의 자리로 만들기
     if N % 10**i // 10 ** (i - 1) >= 5:
+        # 올림하고 0으로 만들기
         N += 10**i - (N % 10**i)
     else:
+        # 내림하고 0으로 만들기
         N -= N % 10**i
 print(N)
