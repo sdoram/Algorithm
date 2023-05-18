f1a62c3cb68596c012b51bbe47e604811d5ad903
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-18 14:54:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,17 @@
+def solution(n, arr1, arr2):
+    # 지도는 한 변의 길이가 n인 정사각형
+    # 벽은 공백 or #으로 이루어짐
+    # 지도1과 2를 겹쳐서 하나라도 벽이면 전체 지도에서 벽
+    # arr = 0은 공백, 1은 벽으로 표현된 이진수를 10진수로 변환한 값 
+    
+    # 각각의 arr의 원소를 2진수로 변환하기 
+    # arr1과 arr2가 아무나 1이라면 1 아니라면 0
+    # 채우기
+    answer = []
+    for i in range(n):
+        bin_ar = f"{int(bin(arr1[i])[2:]) + int(bin(arr2[i])[2:]):0>{n}}"
+        bin_ar = bin_ar.replace('0',' ')
+        bin_ar = bin_ar.replace('1','#')
+        bin_ar = bin_ar.replace('2','#')
+        answer.append(bin_ar)
+    return answer
