a66cae2f3084899bc1dd7b935bde19d6d6a201ee
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-18 12:51:28 +0900

    [unrated] Title: 배열 조각하기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
@@ -3,8 +3,7 @@ def solution(arr, query):
     # 홀수 arr에서 auery[i]번 인덱스 제외 query[i]번 인덱스 앞부분을 자르기
     # return은 남은 arr의 배열 
     for i,q in enumerate(query):
-        if i % 2 == 0:
-            arr = arr[:q+1]
-        else:
-            arr = arr[q:]
+        arr = arr[:q+1] if i % 2 == 0 else arr[q:]
+        # else:
+        #     arr = arr[q:]
     return arr
