675e898e5ac59b3a0d5cccc7d3babfed352ed314
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-05-19 12:00:45 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,2 +1,2 @@
 def solution(array, commands):
-    return [sorted(array[commands[i][0]-1:commands[i][1]])[commands[i][2]-1] for i in range(len(commands))]
+    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]
