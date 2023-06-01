f034d10b75e2c92d68da2a5383cb4dc793acac6e
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-06-01 16:04:32 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,4 +1,4 @@
 def solution(a, b):
-    months = [0, 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     days = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
-    return days[(sum(months[:a+1])+b) % 7]
+    return days[(sum(months[:a])+b) % 7]
