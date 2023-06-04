aeb5fbd23f2fc074940264ce2d4404320b9ac269
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-06-04 23:03:37 +0900

    [Silver IV] Title: 키보드 이벤트, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17254. 키보드 이벤트/키보드 이벤트.py
+++ 백준/Silver/17254. 키보드 이벤트/키보드 이벤트.py
@@ -1,4 +1,2 @@
 N, M = map(int, input().split())
-input_list = [input().split() for _ in range(M)]
-input_list = sorted(input_list, key=lambda x: [int(x[1]), int(x[0])])
-print("".join([x[2] for x in input_list]))
+print("".join([x[2]for x in sorted([input().split() for _ in range(M)], key=lambda x: [int(x[1]), int(x[0])])]))
