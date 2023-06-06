f9cbc30cdc917ebd5e0aae2d8ef7bd3263fa4843
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-06-06 21:39:21 +0900

    [Silver V] Title: 3의 배수, Time: 180 ms, Memory: 41408 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.py
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -3,5 +3,4 @@ COUNT = 0
 while len(X) > 1:
     X = [int(n) for n in str(sum(X))]
     COUNT += 1
-print(COUNT)
-print("YES" if X[0] % 3 == 0 else "NO")
+print(COUNT, "YES" if X[0] % 3 == 0 else "NO", sep="\n")
