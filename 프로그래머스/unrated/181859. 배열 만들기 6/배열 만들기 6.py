2ee196e8c381c6971376095f992ceb1766cf6e93
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-06-08 14:03:22 +0900

    [unrated] Title: 배열 만들기 6, Time: 20.52 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -2,14 +2,13 @@ def solution(arr):
     i = 0
     stk = []
     for _ in range(len(arr)):
-        if i < len(arr):
-            if stk == [] :
-                stk.append(arr[i])
-                i += 1
-            elif stk[-1] == arr[i]:
-                stk.pop()
-                i += 1
-            elif stk[-1] != arr[i]:
-                stk.append(arr[i])
-                i += 1
+        if stk == [] :
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] == arr[i]:
+            stk.pop()
+            i += 1
+        elif stk[-1] != arr[i]:
+            stk.append(arr[i])
+            i += 1
     return stk if stk else [-1]
