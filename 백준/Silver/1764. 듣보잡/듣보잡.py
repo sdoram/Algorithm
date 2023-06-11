12d9314e8168edf0dde7b384761f339cc28afbff
seman_kim <108051577+sdoram@users.noreply.github.com>
2023-06-11 16:16:07 +0900

    [Silver IV] Title: 듣보잡, Time: 3792 ms, Memory: 50120 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -2,7 +2,10 @@ N, M = map(int, input().split())
 persons = {}
 for _ in range(N + M):
     name = input()
-    persons[name] = persons.get(name, 0) + 1
+    if name not in persons:
+        persons[name] = 1
+    else:
+        persons[name] += 1
 unknown_list = [i[0] for i in sorted(persons.items()) if i[1] == 2]
 print(len(unknown_list))
 print("\n".join(unknown_list))
