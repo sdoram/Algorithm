2621b24b8f831ac6e8cfa15baa866a675b041aaf
김세만 <108051577+sdoram@users.noreply.github.com>
2023-06-14 14:21:53 +0900

    [Silver III] Title: 1, Time: 164 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.py
+++ 백준/Silver/4375. 1/1.py
@@ -3,8 +3,7 @@ while True:
         n = int(input())
         i = 1
         while i % n != 0:
-            i *= 10
-            i += 1
+            i = i * 10 + 1
         print(len(str(i)))
     except EOFError:
         break
