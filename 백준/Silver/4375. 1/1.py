77e5cf885903a40af0a426a87448d03c923eec5a
김세만 <108051577+sdoram@users.noreply.github.com>
2023-06-14 14:23:54 +0900

    [Silver III] Title: 1, Time: 160 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.py
+++ 백준/Silver/4375. 1/1.py
@@ -1,9 +1,13 @@
+from sys import stdin
+
 while True:
     try:
-        n = int(input())
-        i = 1
-        while i % n != 0:
-            i = i * 10 + 1
-        print(len(str(i)))
-    except EOFError:
+        n = int(stdin.readline().strip())
+    except ValueError:
         break
+    i = 1
+    COUNT = 1
+    while i % n != 0:
+        i = i * 10 + 1
+        COUNT += 1
+    print(COUNT)
