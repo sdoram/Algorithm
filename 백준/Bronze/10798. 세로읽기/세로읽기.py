38c0f95f0633e9cca0aafc7080e131b92cec781b
김세만 <108051577+sdoram@users.noreply.github.com>
2023-06-17 23:41:57 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,7 @@
+word_list = [[""] * 5 for _ in range(15)]
+for i in range(5):
+    words = list(input())
+    for j, word in enumerate(words):
+        word_list[j][i] = word
+for words in word_list:
+    print("".join(words), end="")
