c6b91dd071849c56c822ffd919d9646d530646eb
김세만 <108051577+sdoram@users.noreply.github.com>
2023-06-19 21:19:37 +0900

    [Bronze I] Title: 약수들의 합, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -3,7 +3,9 @@ while True:
     if n == -1:
         break
     num_list = [num for num in range(1, (n - 1) // 2 + 2) if n % num == 0]
-    if n == sum(num_list):
+    if n == 1:
+        print(f"{n} is NOT perfect.")
+    elif n == sum(num_list):
         print(f"{n} = {' + '.join(map(str, num_list))}")
     else:
         print(f"{n} is NOT perfect.")
