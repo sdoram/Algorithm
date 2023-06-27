79bb8a01c4cbfe7cfeed839cbabb55735a9c9745
김세만 <108051577+sdoram@users.noreply.github.com>
2023-06-27 20:44:01 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -4,6 +4,8 @@ for _ in range(a):
     students = scores.pop(0)
     total_score = sum(scores)
     average_score = total_score // students
-    over_score_raito = format(
-        len([i for i in scores if i > average_score]) / len(scores) * 100, ".3f")
-    print(f'{over_score_raito}%')
+    over_score_raito = list(str(format(
+        len([i for i in scores if i > average_score]) / len(scores) * 100, ".4f")))
+    if int(over_score_raito[-1]) >= 5:
+    	over_score_raito[-2] = str(int(over_score_raito[-2]) + 1)
+    print(f"{''.join(over_score_raito[:-1])}%")
