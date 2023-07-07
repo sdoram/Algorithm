eab08b3f84b9305b64de934b2a4eb0a938970749
김세만 <108051577+sdoram@users.noreply.github.com>
2023-07-07 19:34:52 +0900

    [Silver IV] Title: 암기왕, Time: 1356 ms, Memory: 235616 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -5,5 +5,4 @@ for _ in range(int(stdin.readline())):
     N_word = {i: True for i in list(map(int, stdin.readline().split()))}
     M = int(stdin.readline())
     M_word = list(map(int, stdin.readline().split()))
-    for m in M_word:
-        print(int(N_word.get(m, False)))
+    print("\n".join([str(int(N_word.get(m, False))) for m in M_word]))
