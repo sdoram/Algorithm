6dba76db5b6a08ce225123daec8f742533c3b54b
김세만 <108051577+sdoram@users.noreply.github.com>
2023-07-11 14:55:15 +0900

    [Bronze I] Title: 팀 이름 정하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
+++ 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
@@ -1,16 +1,46 @@
 name = input()
-team_names = []
-for _ in range(int(input())):
-    team_names.append(input())
-winner = {}
-for team in team_names:
-    L = name.count('L')+team.count('L')
-    O = name.count('O')+team.count('O')
-    V = name.count('V')+team.count('V')
-    E = name.count('E')+team.count('E')
-    per = (((L+O) * (L+V) * (L+E) * (O+V) * (O+E) * (V+E)) % 100)
-    if per not in winner:
-        winner[per] = [team]
-    else:
-        winner[per] += [team]
-print(sorted(winner[sorted(winner, reverse=True)[0]])[0])
+L, O, V, E = 0, 0, 0, 0
+for x in name:
+    if x == 'L':
+        L += 1
+    elif x == 'O':
+        O += 1
+    elif x == 'V':
+        V += 1
+    elif x == 'E':
+        E += 1
+LOVE = [L, O, V, E]
+k = int(input())
+maxScore = 0
+fTeamName = ''
+
+for i in range(k):
+    teamName = input()
+    for x in teamName: 
+        if x == 'L':
+            L += 1
+        elif x == 'O':
+            O += 1
+        elif x == 'V':
+            V += 1
+        elif x == 'E':
+            E += 1
+    score = ((L+O) * (L+V) * (L+E) * (O+V) * (O+E) * (V+E)) % 100
+    count = 0
+    if score > maxScore:
+        fTeamName = teamName
+        maxScore = score
+    elif score == maxScore:
+        for i in range(min(len(teamName), len(fTeamName))):
+            if ord(teamName[i]) != ord(fTeamName[i]):
+                if ord(teamName[i]) < ord(fTeamName[i]):
+                    fTeamName = teamName 
+                count = 1
+                break
+        if count == 0:
+            if len(teamName) < len(fTeamName):
+                fTeamName = teamName 
+        if fTeamName == '':
+            fTeamName = teamName
+    L, O, V, E = LOVE
+print(fTeamName)
