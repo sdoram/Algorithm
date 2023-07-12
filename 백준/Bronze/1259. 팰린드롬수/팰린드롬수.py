7dc06dad82e3633c514e5478a2a6682603879665
김세만 <108051577+sdoram@users.noreply.github.com>
2023-07-12 23:45:23 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,15 +1,8 @@
-def palindrome(num):
-    for i1, n1 in enumerate(num):
-        for i2, n2 in enumerate(num[::-1]):
-            if i1 == i2:
-                if n1 != n2:
-                    return 'no'
-
-    return 'yes'
-
-
 while True:
     num = input()
     if num == '0':
         break
-    print(palindrome(num))
+    else:
+        revNum = num[::-1]
+        judge = 'yes' if revNum == num else 'no'
+        print(judge)
