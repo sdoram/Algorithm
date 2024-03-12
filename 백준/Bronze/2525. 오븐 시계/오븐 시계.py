836baefa2ba352ff6c1a736c4a1355c12c9f26bd
김세만 <108051577+sdoram@users.noreply.github.com>
2024-03-12 22:22:50 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,9 +1,4 @@
-H, M = map(int, input().split())
-cooking_time = int(input())
-M += cooking_time
-while M > 59:
-    M -= 60
-    H += 1
-if H > 23:
-    H -= 24
-print(H, M)
+A, B = map(int, input().split())
+C = int(input())
+T = (A * 60 + B + C) % 1440
+print(T // 60, T % 60)
