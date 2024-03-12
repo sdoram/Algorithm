142aff0129571696244cf8dc619eb3a99972d9fe
김세만 <108051577+sdoram@users.noreply.github.com>
2024-03-12 23:10:46 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,8 +1,8 @@
-n = int(input())
-for _ in range(n):
-    num, word = input().split()
-    num = int(num)
-    new_word = ''
-    for w in word:
-        new_word += w * num
-    print(new_word)
+import sys
+for _ in range(int(sys.stdin.readline())):
+    R, S = sys.stdin.readline().split()
+    P = ''
+    for i in S:
+        P += int(R)*i
+    print(P)
+    
