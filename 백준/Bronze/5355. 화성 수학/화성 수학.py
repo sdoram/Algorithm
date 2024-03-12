20c615efa5e996e078ca0ed6c0ab07411d7b9534
김세만 <108051577+sdoram@users.noreply.github.com>
2024-03-12 23:03:33 +0900

    [Bronze II] Title: 화성 수학, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5355. 화성 수학/화성 수학.py
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -1,16 +1,11 @@
-import sys
-T = int(sys.stdin.readline())
-for _ in range(T):
-    num = list(sys.stdin.readline().split())
-    answer = 0
-    for n in num:
-        try:
-            answer += float(n)
-        except ValueError:
-            if n == '@':
-                answer *= 3
-            elif n == '%':
-                answer += 5
-            else:
-                answer -= 7
-    print("{:.2f}".format(answer))
+for t in range(int(input())):
+    A = list(input().split())
+    ANSWER = float(A[0])
+    for i in A:
+        if i == '@':
+            ANSWER *= 3
+        elif i == '%':
+            ANSWER += 5
+        elif i == '#':
+            ANSWER -= 7
+    print(f'{ANSWER:.2f}')
