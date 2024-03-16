991e2c5526aa8514c2b9ac7e5d19e68f055e1a17
김세만 <108051577+sdoram@users.noreply.github.com>
2024-03-16 23:25:34 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,18 +1,11 @@
-# 같은 눈 3개 10,000+(주사위 눈)*1000
-# 같은 눈 2개 1,000+(주사위 눈)*100
-# 같은 눈 없음 (가장 큰 눈)*100
-
-num_list = input().split()
-num_list.sort()
-a = int(num_list[0])
-b = int(num_list[1])
-c = int(num_list[2])
-
-if a == b == c:
-    print(10000+(a*1000))
-elif a == b or a == c:
-    print(1000+(a*100))
-elif b == c:
-    print(1000+(b*100))
+import sys
+A, B, C = map(int,sys.stdin.readline().split())
+if A == B == C:
+    print(10000+A*1000)
+elif A == B or A == C:
+    print(1000+A*100)
+elif B == C:
+    print(1000+B*100)
 else:
-    print(c*100)
+    print(max(A,B,C)*100)
+    
