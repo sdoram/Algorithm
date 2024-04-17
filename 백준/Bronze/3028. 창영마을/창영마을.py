c6741c3009fa009271162f62f2128f01efeab393
김세만 <108051577+sdoram@users.noreply.github.com>
2024-04-17 22:15:31 +0900

    [Bronze II] Title: 창영마을, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3028. 창영마을/창영마을.py
+++ 백준/Bronze/3028. 창영마을/창영마을.py
@@ -3,18 +3,12 @@ MIX_LIST = sys.stdin.readline().rstrip()
 CURRENT_LOCATION = 1
 for c in MIX_LIST:
     if CURRENT_LOCATION == 1:
-        if c == 'A':
-            CURRENT_LOCATION += 1
-        elif c == 'C':
-            CURRENT_LOCATION += 2
+        if c != 'B':
+            CURRENT_LOCATION += 1 if c == 'A' else 2
     elif CURRENT_LOCATION == 2:
-        if c == 'A':
-            CURRENT_LOCATION -= 1
-        elif c == 'B':
-            CURRENT_LOCATION += 1
+        if c != 'C':
+            CURRENT_LOCATION += -1 if c == 'A' else 1
     else:
-        if c == 'B':
-            CURRENT_LOCATION -= 1
-        elif c == 'C':
-            CURRENT_LOCATION -= 2
+        if c != 'A':
+            CURRENT_LOCATION += -1 if c == 'B' else -2
 print(CURRENT_LOCATION)
