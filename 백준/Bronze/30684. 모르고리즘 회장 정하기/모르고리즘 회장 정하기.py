c2dd54ffb11cce7a17fe23cb6320e909644f0a7c
김세만 <108051577+sdoram@users.noreply.github.com>
2024-04-24 22:23:48 +0900

    [Bronze II] Title: 모르고리즘 회장 정하기, Time: 64 ms, Memory: 38044 KB -BaekjoonHub

--- 백준/Bronze/30684. 모르고리즘 회장 정하기/모르고리즘 회장 정하기.py
+++ 백준/Bronze/30684. 모르고리즘 회장 정하기/모르고리즘 회장 정하기.py
@@ -1,5 +1,4 @@
 import sys
 input = sys.stdin.readline
 NAME_LIST = [input().rstrip() for _ in range(int(input()))]
-PRESIDENT_NAME = sorted([n for n in NAME_LIST if len(n) == 3])[0]
-print(PRESIDENT_NAME)
+print(sorted([n for n in NAME_LIST if len(n) == 3])[0])
