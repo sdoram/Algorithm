02303e6d65adad32cdbd43d93c8ce94a95753eb8
김세만 <108051577+sdoram@users.noreply.github.com>
2024-05-14 21:37:50 +0900

    [Bronze II] Title: 자기복제수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2028. 자기복제수/자기복제수.py
+++ 백준/Bronze/2028. 자기복제수/자기복제수.py
@@ -2,4 +2,4 @@ import sys
 input = sys.stdin.readline
 for _ in range(int(input())):
     N = int(input())
-    print('YES' if N**2 % (2*5)**len(str(N)) == N else 'NO')
+    print('YES' if N**2 % 10**len(str(N)) == N else 'NO')
