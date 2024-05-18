6b3282a3dc9730f4ffc554860a11547c5f505c81
김세만 <108051577+sdoram@users.noreply.github.com>
2024-05-18 17:18:05 +0900

    [Silver V] Title: 경매, Time: 136 ms, Memory: 40624 KB -BaekjoonHub

--- 백준/Silver/2238. 경매/경매.py
+++ 백준/Silver/2238. 경매/경매.py
@@ -3,7 +3,6 @@ input = sys.stdin.readline
 U, N = map(int, sys.stdin.readline().split())
 
 def auction_system(U, N):
-    bid = []
     bid_range = dict()
     for _ in range(N):
         S, P = input().split()
@@ -12,8 +11,7 @@ def auction_system(U, N):
             bid_range[P] += [S]
         else:
             bid_range[P] = [S]
-        bid.append([S, P])
-    # 같은 가격을 제시한 사람의 숫자로 오름차순 정렬 후, 낮은 가격으로 다시 오름차순 정렬
+    # 같은 가격을 제시한 사람의 숫자로 오름차순 정렬 후, 낮은 가격으로 다시 오름차순정렬
     successful_bid = sorted(bid_range.items(), key=lambda x:(len(x[1]), x[0]))[0]
     return print(successful_bid[1][0], successful_bid[0])
 
