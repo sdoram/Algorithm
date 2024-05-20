3d7ab31a0c5cf98600027bf055c26166ede9f54b
김세만 <108051577+sdoram@users.noreply.github.com>
2024-05-20 21:34:47 +0900

    [Bronze I] Title: 이진수 나눗셈, Time: 424 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/22950. 이진수 나눗셈/이진수 나눗셈.py
+++ 백준/Bronze/22950. 이진수 나눗셈/이진수 나눗셈.py
@@ -1,7 +1,2 @@
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-M = int(input(), 2)
-K = int(input())
-print("YES" if M % 2**K == 0 else "NO")
+input()
+print("YES" if int(input(), 2) % 2**int(input()) == 0 else "NO")
