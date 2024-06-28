6e9506eba6ef3da669d0dfa9adbc284234b18a7e
김세만 <108051577+sdoram@users.noreply.github.com>
2024-06-28 23:29:49 +0900

    [Bronze II] Title: 준석이의 사탕 사기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28062. 준석이의 사탕 사기/준석이의 사탕 사기.py
+++ 백준/Bronze/28062. 준석이의 사탕 사기/준석이의 사탕 사기.py
@@ -3,7 +3,5 @@ input()
 candy = sorted(map(int, input().split()), reverse=True)
 even_candy = sum([c for c in candy if c % 2 == 0])
 odd_candy = [c for c in candy if c % 2 != 0]
-if len(odd_candy) % 2 != 0:
-    print(even_candy + sum(odd_candy)-odd_candy[-1])
-else:
-    print(even_candy + sum(odd_candy))
+
+print(even_candy + sum(odd_candy) if sum(odd_candy) % 2 == 0 else even_candy + sum(odd_candy)-odd_candy[-1])
