aed5acb351272e862a32f927d418ab09c385ab45
김세만 <108051577+sdoram@users.noreply.github.com>
2024-07-06 19:51:05 +0900

    [Silver V] Title: 성적 통계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5800. 성적 통계/성적 통계.py
+++ 백준/Silver/5800. 성적 통계/성적 통계.py
@@ -1,4 +1,4 @@
 for i in range(1, int(input())+1):
     N = sorted(list(map(int, input().split()))[1:])
     print(f'Class {i}')
-    print(f'Max {max(N)}, Min {min(N)}, Largest gap {max([abs(N[i-1]-n) for i, n in enumerate(N) if i != 0])}')
+    print(f'Max {max(N)}, Min {min(N)}, Largest gap {max([n-N[i-1] for i, n in enumerate(N) if i != 0])}')
