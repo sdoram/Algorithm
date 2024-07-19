ad93b4c421c04be292666d04005b6003f20fe8b9
김세만 <108051577+sdoram@users.noreply.github.com>
2024-07-19 21:48:33 +0900

    [Silver V] Title: 반지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5555. 반지/반지.py
+++ 백준/Silver/5555. 반지/반지.py
@@ -4,4 +4,4 @@ input = sys.stdin.readline
 word = input().rstrip()
 
 rings = [input().rstrip() for _ in range(int(input()))]
-print(sum([1 if word in r[len(r)-len(word)+1:]+r else 0 for r in rings]))
+print(sum([1 if word in r[11-len(word):]+r else 0 for r in rings]))
