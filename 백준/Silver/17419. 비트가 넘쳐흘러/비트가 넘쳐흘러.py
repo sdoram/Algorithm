7ef8c51477bcc07967f81b3f37dde30a89f704b1
김세만 <108051577+sdoram@users.noreply.github.com>
2024-07-31 20:34:01 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 52 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.py
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.py
@@ -1,11 +1,2 @@
-def bit():
-    N = int(input())
-    K = int(input(),2)
-    count = 0
-    
-    while True:
-        if K == 0:
-            return count
-        K = K-(K&((~K)+1))
-        count += 1
-print(bit())
+input()
+print(input().count('1'))
