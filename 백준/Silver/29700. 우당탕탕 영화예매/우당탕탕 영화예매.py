611fcde777cc7413394d032a757997064cd99464
김세만 <108051577+sdoram@users.noreply.github.com>
2024-08-01 22:08:39 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 1016 ms, Memory: 71400 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.py
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.py
@@ -1,4 +1,3 @@
-N, M, K = map(int, input().split())
-
-seat =  [input().split('1') for _ in range(N)]
-print(sum([sum([s.count('0')-K+1 for s in S if s.count('0'*K) >= 1]) for S in seat]))
+N,M,K = map(int, input().split())
+X = [input().split('1') for _ in range(N)]
+print(sum([sum([Z.count('0')-K+1 for Z in Y if Z.count('0'*K) > 0]) for Y in X]))
