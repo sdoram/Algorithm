bc8bef816f890c84f01f07d5c16ec8f53afd97b0
김세만 <108051577+sdoram@users.noreply.github.com>
2024-08-17 23:42:50 +0900

    [Silver IV] Title: 가장 긴 단어, Time: 100 ms, Memory: 34760 KB -BaekjoonHub

--- 백준/Silver/5637. 가장 긴 단어/가장 긴 단어.py
+++ 백준/Silver/5637. 가장 긴 단어/가장 긴 단어.py
@@ -10,11 +10,16 @@ while True:
     word = ''
     for s in sentence:
         if s not in alphabet:
+            # 각 길이별 처음 나온 단어만 저장
             if len(word) not in word_dict:
                 word_dict[len(word)] = word.lower()
             word = ''
         else:
             word += s
+    if len(word) not in word_dict and word != 'E-N-D':
+        word_dict[len(word)] = word.lower()
+
+    # 마지막부분이 E-N-D인지 확인
     if 'E-N-D' == sentence[-5:]:
         break
 print(word_dict[max(word_dict)])
