16a63500ec88303d2bd374f67a257897acf97478
김세만 <108051577+sdoram@users.noreply.github.com>
2024-09-09 01:49:36 +0900

    [Silver IV] Title: PRASE, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3022. PRASE/PRASE.py
+++ 백준/Silver/3022. PRASE/PRASE.py
@@ -1,7 +1,7 @@
 warning_count = 0
 pickup_list = [input() for _ in range(int(input()))]
 children = {pickup:0 for pickup in pickup_list}
-for i, pickup in enumerate(pickup_list, start=1):
+for pickup in pickup_list:
     if sum(children.values()) - children[pickup] < children[pickup]:
         warning_count += 1
     children[pickup] += 1
