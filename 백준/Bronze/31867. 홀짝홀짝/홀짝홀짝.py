becfe88898df6741e372010034fb7d06d279f1bb
김세만 <108051577+sdoram@users.noreply.github.com>
2024-10-17 15:59:45 +0900

    [Bronze IV] Title: 홀짝홀짝, Time: 248 ms, Memory: 48048 KB -BaekjoonHub

--- 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.py
+++ 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.py
@@ -1,8 +1,9 @@
 input()
 K = list(map(int, list(input())))
-if sum([1 for k in K if k % 2 == 0]) > sum([1 for k in K if k % 2 != 0]):
-    print(0)
-elif sum([1 for k in K if k % 2 == 0]) < sum([1 for k in K if k % 2 != 0]):
-    print(1)
-else:
+if sum([1 if k % 2 == 0 else -1 for k in K ]) == 0:
     print(-1)
+elif sum([1 if k % 2 == 0 else -1 for k in K ]) > 0:
+    print(0)
+else :
+    print(1)
+    
