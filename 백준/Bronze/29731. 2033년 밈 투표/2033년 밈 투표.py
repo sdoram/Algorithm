531a8895a6b6f3475d5934b418b946d96078ec99
김세만 <108051577+sdoram@users.noreply.github.com>
2025-07-03 23:17:48 +0900

    [Bronze IV] Title: 2033년 밈 투표, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.py
@@ -0,0 +1,16 @@
+X = [   
+'Never gonna give you up',
+'Never gonna let you down',
+'Never gonna run around and desert you',
+'Never gonna make you cry',
+'Never gonna say goodbye',
+'Never gonna tell a lie and hurt you',
+'Never gonna stop'
+]
+
+check = False
+for _ in range(int(input())):
+    if input() not in X:
+        check = True
+        
+print('Yes' if check else 'No')
